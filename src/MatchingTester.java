dc31761b4c0523490986d3c1e63b95b71fe278af
kkh0920 <rudgns1535@gmail.com>
2024-05-30 11:27:59 +0900

    matching test

--- src/MatchingTester.java
+++ src/MatchingTester.java
@@ -25,25 +25,70 @@ public class MatchingTester {
     
     public static final int DEPARTMENT_CAPACITY = 40;
 
+    public static final int[] APPLICANTS = { 45, 28, 35, 46, 39, 20, 63, 20, 37, 20, 
+                                            12, 31, 26, 36, 14, 25, 23, 37, 145, 103,
+                                            33, 48, 33, 31, 50 }; // 각 학과 별 지원 현황                     
+
     public static void main(String[] args) {
-        matching();
+        List<Department> departments = new LinkedList<>();
+        List<Student> students = new LinkedList<>();
+        
+        addDepartments(departments, DEPARTMENT);
+        addRandomStudents(students, departments, STUDENT);
+
+        MatchingManager matcher = new MatchingManager(students, departments);
+        List<Student> matchedStudent = matcher.matching(0.7, 1.3); // matching
+
+        print(matcher, matchedStudent, departments);
+    }
+
+    public static void print(MatchingManager matcher, List<Student> matchedStudent, List<Department> departments) {
+        System.out.print("\n\n-------------------  Matching Result  -------------------\n\n");
+        int i = 0;
+        for(Department department : departments) {
+            System.out.printf("%10s", "학과");
+            System.out.printf("%2s", department.getId());
+            System.out.printf("%10s", "(" + department.getApplicants() + " / " + department.getCapacity() + ")");
+            i++;
+            if(i % 2 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.print("\n\n            매칭된 총 인원");
+        System.out.printf("%16s", " : " + matchedStudent.size() + " / " + STUDENT);
+        System.out.print("\n            선호도 점수 총합" );
+        System.out.printf("%13s", " : " + matcher.getTotalPreference() + " / " + Student.MAX_APPLY * STUDENT);
+        System.out.println("\n\n---------------------------------------------------------\n\n");
+
     }
 
     public static void addRandomStudents(List<Student> students, List<Department> departments, int studentCount) {
+        int remain = DEPARTMENT;      
+        for(int i = 0; i < DEPARTMENT; i++) {
+            APPLICANTS[i] *= Student.MAX_APPLY; // 3회 지원
+        }
         Random random = new Random();
         for(int i = 1; i <= studentCount; i++) {
             Student student = new Student(Integer.toString(i));
-            double grade = random.nextInt(46) / 10.0; // 성적 : 0.0 ~ 4.5
-            
-            // 랜덤 성적 설정
-            student.setGrade(Math.round(grade * 100) / 100.0);
 
-            // 랜덤 학과, 랜덤 횟수 지원 (최대 Student.MAX_APPLY)
-            int applyCnt = random.nextInt(Student.MAX_APPLY) + 1;
-            for(int j = 0; j < applyCnt; j++) {
-                int randomIndex = random.nextInt(departments.size());
-                String preferDepartment = departments.get(randomIndex).getId();
-                student.addPreferred(preferDepartment);
+            // 랜덤 성적 설정 (0.0 ~ 4.5)
+            double grade = random.nextInt(46) / 10.0; 
+            student.setGrade(Math.round(grade * 100) / 100.0);
+            
+            // 랜덤 학과 지원
+            int apply = remain > 3 ? 3 : remain;
+            for(int j = 0; j < apply; j++) {
+                int randomIndex;
+                String preferDepartment;
+                do {
+                    randomIndex = random.nextInt(departments.size());
+                    preferDepartment = departments.get(randomIndex).getId();
+                } while(APPLICANTS[randomIndex] <= 0 ||
+                            !student.addPreferred(preferDepartment)); // 학과 중복 등록 방지
+
+                if((--APPLICANTS[randomIndex]) == 0) {
+                    remain--;
+                }
             }
             
             students.add(student);
@@ -54,58 +99,6 @@ public class MatchingTester {
         for(int i = 1; i <= departmentCount; i++) {
             Department department = new Department(Integer.toString(i), DEPARTMENT_CAPACITY);
             departments.add(department);
-        }
-    }
-
-    public static void printOrderByGrade(List<Student> matchedStudent) {
-        Collections.sort(matchedStudent);
-        for(Student student : matchedStudent) {
-            System.out.println("학생" + student.getStudentID() + 
-                                " (성적 : " + student.getGrade() + 
-                                " / 학과 : " + student.getMatchedDepartment() + ")");
-        }
-    }
-
-    public static void matching() {
-        List<Department> departments = new LinkedList<>();
-        addDepartments(departments, DEPARTMENT);
-
-        List<Student> students = new LinkedList<>();
-        addRandomStudents(students, departments, STUDENT);
-
-        System.out.println();
-        System.out.println();
-        System.out.println("-------------------  Matching Result  -------------------");
-
-        MatchingManager matcher = new MatchingManager(students, departments);
-        List<Student> matchedStudent = matcher.matching(0.7, 1.3);
-        print(departments);
-        
-        System.out.println();
-        System.out.println();
-        System.out.print("            매칭된 총 인원");
-        System.out.printf("%16s", " : " + matchedStudent.size() + " / " + STUDENT);
-        System.out.println();
-        System.out.print("            선호도 점수 총합" );
-        System.out.printf("%13s", " : " + matcher.getTotalPreference() + " / " + Student.MAX_APPLY * STUDENT);
-        System.out.println();
-        System.out.println();
-        System.out.println("---------------------------------------------------------");
-        System.out.println();
-        System.out.println();
-    }
-
-    public static void print(List<Department> departments) {
-        int i = 0;
-        System.out.println();
-        for(Department department : departments) {
-            System.out.printf("%10s", "학과");
-            System.out.printf("%2s", department.getId());
-            System.out.printf("%10s", "(" + department.getApplicants() + " / " + department.getCapacity() + ")");
-            i++;
-            if(i % 2 == 0) {
-                System.out.println();
-            }
         }
     }
 }
